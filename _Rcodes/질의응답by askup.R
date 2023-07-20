649d4cf823c7403e70a1a94a1d221ff99fcc77e2
Kim JinSeong <131634612+Jinseongmensch@users.noreply.github.com>
2023-07-20 19:22:31 +0900

    Add files via upload

---
+++ _Rcodes/질의응답by askup.R
@@ -0,0 +1,4 @@
+#Residuals vs Fitted plot은 종속변수와 독립변수간에 선형성을 알아볼 수 있습니다.
+# Normal Q-Q plot은 잔차가 정규분포를 따르는지 확인할 수 있습니다.
+# Scale-Location plot은 잔차와 적합값 사이에 동일한 분산성을 가지는지 확인할 수 있습니다.
+# Residuals vs Leverage plot은 이상치나 극단값이 있는지 확인할 수 있습니다. 
